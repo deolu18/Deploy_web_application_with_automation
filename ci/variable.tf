387b595cbdc150bb820fcf16eea4a2d9426e5e01
deolu18 <41289746+deolu18@users.noreply.github.com>
2020-02-07 18:06:41 +0000

    Add files via upload

---
+++ ci/variable.tf
@@ -0,0 +1,34 @@
+variable region{
+  default = "eu-west-1"
+}
+variable account{
+  default = ""
+}
+variable workload{
+  default = "wordpressApplication"
+}
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+variable "private_key_word" {
+  default = "mykey"
+}
+
+variable "public_key_word" {
+  default = "mykey.pub"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    eu-west-1 = ""
+  }
+}
+
+variable "instance_device_storage" {
+  default = "/dev/xvdh"
+}
+
+variable "jenkins_vers" {
+  default = "2.204.1"
+}
