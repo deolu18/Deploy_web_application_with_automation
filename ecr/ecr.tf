b8ea7706d6805d1e6d757c433ae5a4d32e986f69
deolu18 <41289746+deolu18@users.noreply.github.com>
2020-02-27 18:32:45 +0000

    Update ecr.tf

--- ecr/ecr.tf
+++ ecr/ecr.tf
@@ -1,3 +1,16 @@
 resource "aws_ecr_repository" "wordpress_repo" {
   name = "adeoluwordpress"
 }
+
+
+resource "aws_ecs_cluster" "wordcluster" {
+  name = "wordprApp"
+}
+
+
+resource "aws_ecs_service" "wordservice" {
+  name            = "wordpressservice"
+  cluster         = "${aws_ecs_cluster.wordcluster.id}"
+  task_definition = "${aws_ecs_task_definition.word-press.arn}"
+  desired_count   = 1
+}
